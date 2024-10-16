a156a67f77192c98141a1db00f6576b19b46e4e7
Adam Silkey <Adam.Silkey@gmail.com>
2024-10-16 09:29:08 -0500

    Add Dependncies.lua

---
+++ src/Dependencies.lua
@@ -0,0 +1,16 @@
+--[[
+    Memories of Valor
+
+    Author: Adam Silkey
+    Email: Adam.Silkey@gmail.com
+
+    Strategy RPG Game, made for GD50 Final Project
+]]
+
+---
+--- LIBRARIES ------------------------------------------------------------------
+---
+
+Class = require 'lib/class'
+push = require 'lib/push'
+Timer = require 'lib/knife.timer'
