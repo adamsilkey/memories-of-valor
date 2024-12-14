c625e18c5e371c3b16ba5ee73dfe1a0734810aba
Adam Silkey <Adam.Silkey@gmail.com>
2024-12-13 19:49:53 -0600

    Rename MapTileset.tsx -> WorldTileset.tsx
    Update references

---
+++ tiled/WorldTileset.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="World" tilewidth="16" tileheight="16" tilecount="400" columns="20">
+ <editorsettings>
+  <export target="../maps/MapTileset.lua" format="lua"/>
+ </editorsettings>
+ <image source="../assets/tiles/WorldTileset.png" width="320" height="320"/>
+</tileset>
