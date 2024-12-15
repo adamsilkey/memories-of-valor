27047223c89ba90b8daf5cf9e4ed74e88ad9316c
Adam Silkey <Adam.Silkey@gmail.com>
2024-12-15 07:13:53 -0600

    Implement visuals for BattleActionMenu
    - Add icons to BattleActionMenu
    - Enable grayscale shading for 'nonactive' menu items
    - Add bold border to 'active' item (to help colorblind people)
    - Add ability to change Panel() color

--- src/ui/BattleActionMenu.lua
+++ src/ui/BattleActionMenu.lua
@@ -16,25 +16,102 @@
 ---@class BattleActionMenu
 BattleActionMenu = Class{}
 
+local SCALE_FACTOR = 0.45
+
+---@enum BattleActionMenu.DIRS
+BattleActionMenu.DIRS = {
+    N = 'N',
+    S = 'S',
+    E = 'E',
+    W = 'W',
+}
+
 function BattleActionMenu:init()
 
-    self.width = 40
-    self.height = 40
+    -- Everything about the four panels generated below is dynamic to make
+    -- a nice symmmetrical little window at the bottom of the screen
+
+    self.miniWidth = 16
+    self.miniHeight = 24
+
+    self.width = self.miniWidth * 3
+    self.height = self.miniHeight * 2
 
     self.x = math.floor((VIRTUAL_WIDTH / 2) - (self.width / 2))
     self.y = math.floor(VIRTUAL_HEIGHT - self.height)
 
     --- To hold the four square panels
     self.panels = {
-        N = Panel(self.x, self.y, self.width, self.height)
+        -- This was a debug panel usd to try and gauge the oerall size
+        -- BIGGIE = Panel(self.x, self.y, self.width, self.height),
+
+        --- Panel 1 - Top - x=(self.width / 2) -  (self.miniWidth / 2), y=0
+        N = self:createMiniPanel(
+            math.floor((self.width / 2) - (self.miniWidth / 2)),
+            0,
+            ICONS.BATTLE_MENU_SWORD
+        ),
+        --- Panel 2 - Bottom - x=(smae as Panel 1), y = self.y + miniHeight
+        S = self:createMiniPanel(
+            math.floor((self.width / 2) - (self.miniWidth / 2)),
+            self.miniHeight,
+            ICONS.BATTLE_MENU_WALK
+        ),
+        --- Panel 3 - Left
+        W = self:createMiniPanel(
+            0,
+            math.floor((self.height / 2) - (self.miniHeight / 2)),
+            ICONS.BATTLE_MENU_MAGIC
+        ),
+        --- Panel 4 - Right
+        E = self:createMiniPanel(
+            self.miniWidth * 2,
+            math.floor((self.height / 2) - (self.miniHeight / 2)),
+            ICONS.BATTLE_MENU_ITEM
+        ),
     }
+
+    self.currentPanel = self.DIRS.N
+
 end
 
 function BattleActionMenu:update(dt)
+    if love.keyboard.isDown(KEYS.LEFT, KEYS.A) then
+        self.currentPanel = self.DIRS.W
+    elseif love.keyboard.isDown(KEYS.RIGHT, KEYS.D) then
+        self.currentPanel = self.DIRS.E
+    elseif love.keyboard.isDown(KEYS.UP, KEYS.W) then
+        self.currentPanel = self.DIRS.N
+    elseif love.keyboard.isDown(KEYS.DOWN, KEYS.D) then
+        self.currentPanel = self.DIRS.S
+    end
 end
 
 function BattleActionMenu:render()
     for name, panel in pairs(self.panels) do
-        panel:render()
+        if name ~= self.currentPanel then
+            panel:render({dimIcon = true})
+        else
+            panel:render({boldBorder = true})
+        end
     end
+end
+
+function BattleActionMenu:createMiniPanel(offsetX, offsetY, icon)
+    local x = math.floor(self.x + offsetX)
+    local y = math.floor(self.y + offsetY)
+
+    ---@type PanelDef
+    local panelDef = {
+        x=x,
+        y=y,
+        width=self.miniWidth,
+        height=self.miniHeight,
+        icon=icon,
+        iconXoffset=1,
+        iconYoffset=4,
+        iconScaleFactor=SCALE_FACTOR,
+    }
+
+    return Panel(panelDef)
 end
--- src/ui/Panel.lua
+++ src/ui/Panel.lua
@@ -10,31 +10,140 @@
 ---@class Panel
 Panel = Class{}
 
----@param x posX
----@param y posY
----@param width iPixels
----@param height iPixels
-function Panel:init(x, y, width, height)
-    self.x = x
-    self.y = y
-    self.width = width
-    self.height = height
+---@class PanelDef
+---@field x posX
+---@field y posY
+---@field width iPixels
+---@field height iPixels
+---@field color? RGB
+---@field borderColor? RGB
+---@field icon? ICONS Unique Icon Frame ID
+---@field iconXoffset iPixels
+---@field iconYoffset iPixels
+---@field iconScaleFactor? number
+
+---@param def PanelDef
+function Panel:init(def)
+    self.x = def.x
+    self.y = def.y
+    self.width = def.width
+    self.height = def.height
+
+    self.color = def.color
+    if self.color == nil then
+        self.color = {
+            r = 56 / 255,
+            g = 56 / 255,
+            b = 56 / 255,
+            a = 1,
+        }
+    end
+
+    self.borderColor = def.borderColor
+    if self.borderColor == nil then
+        self.borderColor = {
+            r = 0.8,
+            g = 0.8,
+            b = 0.8,
+            a = 1,
+        }
+    end
+
+    self.icon = def.icon
+    self.iconXoffset = def.iconXoffset or 0
+    self.iconYoffset = def.iconYoffset or 0
+    self.iconScaleFactor = def.iconScaleFactor or 1
 
     ---@type boolean Flag to toggle if the panel is visible or not
     self.visible = true
+
+    -- Found examples of how to do this in various places
+    -- Played around with it until I landed on the approach of using dot/mix
+    -- https://forum.derivative.ca/t/glsl-to-tint-greyscale-image-to-color/134255
+    -- https://gist.github.com/Volcanoscar/4a9500d240497d3c0228f663593d167a
+    --
+    -- Gray shader that will turn a sprite into grayscale when used
+    ---@type love.Shader
+    self.grayShader = love.graphics.newShader[[
+        extern float grayFactor;
+
+        vec4 effect(vec4 vcolor, Image tex, vec2 texcoord, vec2 pixcoord)
+        {
+            vec4 outputcolor = Texel(tex, texcoord) * vcolor;
+            float gray = dot(outputcolor.rgb, vec3(0.3, 0.6, 0.1));
+            outputcolor.rgb = mix(outputcolor.rgb, vec3(gray), grayFactor);
+            return outputcolor;
+        }
+    ]]
 end
 
 function Panel:update(dt)
 
 end
 
-function Panel:render()
+---@class PanelRenderDefs
+---@field dimIcon? boolean
+---@field boldBorder? boolean
+
+---@param params? table
+function Panel:render(params)
+    if params == nil then
+        params = {}
+    end
+
+    local dimIcon = params.dimIcon
+    if dimIcon == nil then
+        dimIcon = false
+    end
+
+    local boldBorder = params.boldBorder
+    if boldBorder == nil then
+        boldBorder = false
+    end
+
     if self.visible then
-        love.graphics.setColor(1, 1, 1, 1)
+        love.graphics.setColor(
+            self.borderColor.r,
+            self.borderColor.g,
+            self.borderColor.b,
+            self.borderColor.a
+        )
         love.graphics.rectangle('fill', self.x, self.y, self.width, self.height, 3)
-        love.graphics.setColor(56/255, 56/255, 56/255, 1)
+        love.graphics.setColor(
+            self.color.r,
+            self.color.g,
+            self.color.b,
+            self.color.a
+        )
         love.graphics.rectangle('fill', self.x + 1, self.y + 1, self.width - 2, self.height - 2, 3)
         love.graphics.setColor(1, 1, 1, 1)
+
+        if boldBorder then
+            -- Render Bold Border
+            love.graphics.setColor(1, 1, 1, 1)
+            -- draw actual cursor rect
+            love.graphics.setLineWidth(2)
+            love.graphics.rectangle('line', self.x, self.y, self.width, self.height, 3)
+        end
+
+
+        if self.icon ~= nil then
+            if dimIcon then
+                love.graphics.setShader(self.grayShader)
+                self.grayShader:send("grayFactor", 1)
+            end
+            love.graphics.draw(
+                Textures[GFX.UI.ICONS],
+                Frames[GFX.UI.ICONS][self.icon],
+                self.x + self.iconXoffset,
+                self.y + self.iconYoffset,
+                0,
+                self.iconScaleFactor,
+                self.iconScaleFactor
+            )
+            -- Reset the shader
+            love.graphics.setShader()
+        end
     end
 end
 
